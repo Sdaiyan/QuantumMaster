a5f1470c3479d822f6c3519a93054637d5fbce4b
Sdaiyan <daishan526@gmail.com>
2025-05-16 14:00:08 +0800

    feat: 结构更新

--- src/QuantumMaster.cs
+++ src/QuantumMaster.cs
@@ -1,10 +1,8 @@
 ﻿using HarmonyLib;
+using GameData.Domains;
+using GameData.Utilities;
 using TaiwuModdingLib.Core.Plugin;
 using Redzen.Random;
-using GameData;
-using GameData.Utilities;
-using System;
-using System.Collections.Generic;
 
 namespace QuantumMaster
 {
@@ -13,18 +11,35 @@ namespace QuantumMaster
     public class QuantumMaster : TaiwuRemakeHarmonyPlugin
     {
         Harmony harmony;
-        
+
+        public static bool BreakVisible;
+
+        public override void OnModSettingUpdate()
+        {
+            DomainManager.Mod.GetSetting(ModIdStr, "steal", ref BreakVisible);
+        }
         public override void Initialize()
         {
             harmony = new Harmony("daige");
-            RegisterBreakVisiblePatch();
+            patchBreakVisible();
         }
 
-        public void RegisterBreakVisiblePatch()
+        public override void Dispose()
         {
+            if (harmony != null)
+            {
+                harmony.UnpatchSelf();
+                harmony = null;
+            }
+        }
+
+        // 补丁方法
+        public void patchBreakVisible()
+        {
+            if (!BreakVisible) return;
             // 使用预设值创建补丁
             var patchBuilder = GenericTranspiler.CreatePatchBuilder(
-                "BreakVisible", 
+                "BreakVisible",
                 PatchPresets.OriginalMethods.SkillBreakPlateRandomGridData);
 
             // 添加扩展方法替换，使用预设值
@@ -36,8 +51,9 @@ namespace QuantumMaster
             // 应用补丁
             patchBuilder.Apply(harmony);
         }
-    }
 
+    }
+    
     public class RandomPath
     {
         public static int Random_Next_2Args_Max(int min, int max)
@@ -54,7 +70,7 @@ namespace QuantumMaster
         {
             return max - 1;
         }
-        
+
         public static int Random_Next_1Arg_0(int max)
         {
             return 0;
@@ -78,7 +94,7 @@ namespace QuantumMaster
             }
             return true;
         }
-        
+
         public static bool Random_CheckProb_True(IRandomSource randomSource, int chance, int percent)
         {
             if (percent > 0)
